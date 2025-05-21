32f3934513e4ee70628f8ed044206bcafb8b2948
WackyChang768 <crowak.gaess.768@gmail.com>
2025-05-22 00:39:57 +1000

    Add files via upload

--- Java.js
+++ Java.js
@@ -45,3 +45,14 @@
         }
         item.addEventListener('click', onClick)
         }
+
+Fancybox.bind( "[data-fancybox]" );
+document.querySelector( 'h1' ).addEventListener( 'click', showMessage );
+$( 'button' ).click( filterItems );
+function filterItems() {
+    let button = this;
+    let selector = $( button ).data( 'filter' );
+    console.log( selector );
+    $('#products img').hide(500);
+    $(selector).show(500);
+    }
